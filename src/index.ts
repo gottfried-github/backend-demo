c8a2942b565189fc8139f6d2a0f4fd274d1af5bd
gottfried <gottfried.dev@gmail.com>
2024-05-22 11:49:26 +0300

    fix problems with ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('node running')
+import path from 'path'
+import { fileURLToPath } from 'url'
+import express from 'express'
+
+import api from './routers/index.js'
+
+const PORT = parseInt(process.env.PORT as string, 10)
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+function main() {
+  const app = express()
+
+  app.use('api', api)
+  app.use('/', express.static(path.join(__dirname, './public')))
+  app.use('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/404.html'))
+  })
+
+  app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+  })
+}
+
+main()
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,17 +1,21 @@
 import { Router } from 'express'
 import bodyParser from 'body-parser'
 
-import middlewareAuth from '../middleware/auth'
-import auth from './public/auth'
-import users from './secure/users'
-import posts from './secure/posts'
+import middlewareAuth from '../middleware/auth.js'
+import auth from './public/auth.js'
+import users from './secure/users.js'
+import posts from './secure/posts.js'
 
 const router = Router()
 const _bodyParser = bodyParser.json()
-
+//
 router.use(_bodyParser)
 router.use('auth', auth)
 router.use('users', middlewareAuth, users)
 router.use('posts', middlewareAuth, posts)
+router.use('*', (req, res) => {
+  res.status(404)
+  return res.json({ message: 'endpoint not found' })
+})
 
 export default router
