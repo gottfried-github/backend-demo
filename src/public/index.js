67cf5808d30650197526a6bf87b057eacf9f1c52
gottfried <gottfried.dev@gmail.com>
2024-05-23 23:07:47 +0300

    implement a basic UI

--- src/public/index.js
+++ src/public/index.js
@@ -1,65 +1,142 @@
 const state = {}
 
-async function signup() {
-  const res = await fetch(`/api/auth/signup`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      name: 'username00',
-      email: 'email00',
-      password: 'password00',
-    }),
+const Api = {
+  signup: async data => {
+    const res = await fetch(`/api/auth/signup`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    })
+
+    const resBody = await res.json()
+    console.log('signup, response:', resBody)
+
+    state.user = resBody.data
+  },
+
+  signin: async data => {
+    const res = await fetch(`/api/auth/signin`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    })
+
+    const resBody = await res.json()
+    console.log('signin, response:', resBody)
+
+    state.user = resBody.data
+  },
+
+  getUser: async () => {
+    if (!state.user)
+      throw new Error(
+        'user data is not present, you need to sign in or sign up first to get the user id'
+      )
+
+    const res = await fetch(`/api/users/${state.user.id}`)
+
+    const resBody = await res.json()
+    console.log('get user, response:', resBody)
+  },
+
+  createPost: async data => {
+    const res = await fetch(`/api/posts`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    })
+
+    const resBody = await res.json()
+    console.log('create post, response:', resBody)
+  },
+
+  getPosts: async () => {
+    const res = await fetch('/api/posts')
+
+    const resBody = await res.json()
+    console.log('getPosts, response:', resBody)
+  },
+}
+
+const initSignup = () => {
+  const form = document.querySelector('form#signup')
+
+  form.addEventListener('submit', ev => {
+    ev.preventDefault()
+
+    const formData = new FormData(ev.target)
+    console.log('initSignup, formData:', formData)
+
+    const formObject = {}
+
+    for (const [k, v] of formData.entries()) {
+      formObject[k] = v
+    }
+
+    Api.signup(formObject)
   })
-
-  const resBody = await res.json()
-  console.log('signup, response:', resBody)
-
-  state.user = resBody.data
 }
 
-async function signin() {
-  const res = await fetch(`/api/auth/signin`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      email: 'email00',
-      password: 'password00',
-    }),
+const initSignin = () => {
+  const form = document.querySelector('form#signin')
+
+  form.addEventListener('submit', ev => {
+    ev.preventDefault()
+
+    const formData = new FormData(ev.target)
+    console.log('initSignin, formData:', formData)
+
+    const formObject = {}
+
+    for (const [k, v] of formData.entries()) {
+      formObject[k] = v
+    }
+
+    Api.signin(formObject)
   })
-
-  const resBody = await res.json()
-  console.log('signin, response:', resBody)
-
-  state.user = resBody.data
 }
 
-async function getUser() {
-  if (!state.user) throw new Error('user data is not present')
+const initCreatePost = () => {
+  const form = document.querySelector('form#post')
 
-  const res = await fetch(`/api/users/${state.user.id}`)
+  form.addEventListener('submit', ev => {
+    ev.preventDefault()
 
-  const resBody = await res.json()
-  console.log('get user, response:', resBody)
-}
+    const formData = new FormData(ev.target)
+    console.log('initCreatePost, formData:', formData)
 
-async function createPost() {
-  const res = await fetch(`/api/posts`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      body: 'Lorem ipsum dolor amet',
-    }),
+    const formObject = {}
+
+    for (const [k, v] of formData.entries()) {
+      formObject[k] = v
+    }
+
+    Api.createPost(formObject)
   })
-
-  const resBody = await res.json()
-  console.log('create post, response:', resBody)
 }
 
-async function getPosts() {
-  const res = await fetch('/api/posts')
-
-  const resBody = await res.json()
-  console.log('getPosts, response:', resBody)
+const initGetPosts = () => {
+  const button = document.querySelector('button#get-posts')
+  button.addEventListener('click', () => {
+    Api.getPosts()
+  })
 }
 
-console.log('Hello, pecode')
+const initGetUser = () => {
+  const button = document.querySelector('button#get-user')
+  button.addEventListener('click', () => {
+    Api.getUser()
+  })
+}
+
+const main = () => {
+  console.log('Hello, pecode')
+
+  initSignup()
+  initSignin()
+  initCreatePost()
+  initGetPosts()
+  initGetUser()
+}
+
+document.addEventListener('DOMContentLoaded', main)
